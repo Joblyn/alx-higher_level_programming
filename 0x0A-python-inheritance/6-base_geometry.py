be8dcfaabd4a57a37804d59b004698f3e1cc6670
Job Oaikhenah <oaikhenahjob@gmail.com>
2023-09-12 01:13:56 +0000

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+
+Module that describes a class BaseGeometry
+
+"""
+
+
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        raise Exception("area() is not implemented")
