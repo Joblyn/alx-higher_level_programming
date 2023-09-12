61dc8a523915aa56753c9c95956ad523be96f18b
Job Oaikhenah <oaikhenahjob@gmail.com>
2023-09-12 01:29:55 +0000

    my integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+
+Module that describes a class MyInt
+
+"""
+
+class MyInt(int):
+    """ Class that inherits from class int"""
+
+    def __eq__(self, other):
+        """ Method that returns != check """
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """ Method that returns == check """
+        return int.__eq__(self, other)
